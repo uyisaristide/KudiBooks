4491a930b76682062b9b6b7e2bdc61fa61b23889
Elvin-HIRWA <uyisaristide@gmail.com>
2022-06-07 11:30:42 +0200

    new branch commit

---
+++ lib/screens/auth_screens/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/categories/lockIcon.png')
+        ],
+      ),
+    );
+  }
+}
