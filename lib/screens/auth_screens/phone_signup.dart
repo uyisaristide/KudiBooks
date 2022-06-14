4ce2c36f64616415f7d8c318497da81cc5001894
mucyorene <renemucyomucici@gmail.com>
2022-06-14 13:02:44 +0200

    advanced validation, rounded rectangle button changes added

--- lib/screens/auth_screens/phone_signup.dart
+++ lib/screens/auth_screens/phone_signup.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:kudibooks_app/screens/auth_screens/validators/validator.dart';
 import 'package:kudibooks_app/screens/auth_screens/widgets/circled_logo.dart';
@@ -11,84 +9,126 @@ import 'package:kudibooks_app/screens/auth_screens/widgets/phone_input.dart';
 import 'package:kudibooks_app/screens/auth_screens/widgets/text_form_field.dart';
 import 'package:kudibooks_app/screens/background.dart';
 
-class PhoneSignup extends StatelessWidget {
+class PhoneSignup extends StatefulWidget {
   PhoneSignup({Key? key}) : super(key: key);
+
+  @override
+  State<PhoneSignup> createState() => _PhoneSignupState();
+}
+
+class _PhoneSignupState extends State<PhoneSignup> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final phoneNumber = TextEditingController();
+
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final pinController = TextEditingController();
+  final confirmPin = TextEditingController();
+
+  String _countryCodes = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    firstNameController.addListener(() => setState(() {}));
+    lastNameController.addListener(() => setState(() {}));
+    phoneController.addListener(() => setState(() {}));
+    pinController.addListener(() => setState(() {}));
+    confirmPin.addListener(() => setState(() {}));
+  }
 
   @override
   Widget build(BuildContext context) {
     return BackgroundScreen(
-        screens: Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const PageTitle(
-            title: 'Create new account',
-          ),
-          CustomFormField(
-            fieldIcon: const Icon(
-              Icons.person_outline,
-              size: 18,
+      screens: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const PageTitle(
+              title: 'Create new account',
             ),
-            hintText: 'First Name',
-            validators: (value) => Validators.validateName(value),
-          ),
-          CustomFormField(
-            fieldIcon: const Icon(Icons.person, size: 18),
-            hintText: 'Last Name',
-            validators: (value) => Validators.validateName(value),
-          ),
-          PhoneField(
-              fieldIcon: const Icon(Icons.phone, size: 18),
-              phoneNumber: phoneNumber),
-          CustomFormField(
-            fieldIcon: const Icon(Icons.remove_red_eye, size: 18),
-            inputType: TextInputType.number,
-            hintText: 'Pin',
-            validators: (value) => Validators.validatePassword(value),
-          ),
-          CustomFormField(
-            inputType: TextInputType.number,
-            fieldIcon: const Icon(Icons.remove_red_eye, size: 18),
-            hintText: 'Confirm Password',
-            validators: (value) => Validators.validatePassword(value),
-          ),
-          LoginButton(
-            text: 'Register now',
-            validate: () => _formKey.currentState!.validate()
-                ? Navigator.pushNamed(context, '/otp')
-                : null,
-            actionField: () {
-              if (_formKey.currentState!.validate()) {
-                print("Here we go!");
-              }
-            },
-          ),
-          const HyperLinkText(directingText: 'Login instead'),
-          const CustomDevider(middleText: 'Or sign up with'),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-            child: Row(
-              children: const [
-                CircledLogo(
-                  logo: 'assets/images/categories/logoutIcon.png',
-                  navigateTo: '/phoneLogin',
-                ),
-                CircledLogo(
-                  logo: 'assets/images/categories/googleIcon.png',
-                  navigateTo: '/signup',
-                ),
-                CircledLogo(
-                  logo: 'assets/images/categories/appleIcon.png',
-                  navigateTo: '/phoneSignup',
-                )
-              ],
+            CustomFormField(
+              fieldIcon: const Icon(
+                Icons.person_outline,
+                size: 18,
+              ),
+              hintText: 'First Name',
+              validators: (value) => Validators.validateName(value),
+              fieldController: firstNameController,
             ),
-          )
-        ],
+            CustomFormField(
+              fieldIcon: const Icon(Icons.person, size: 18),
+              hintText: 'Last Name',
+              validators: (value) => Validators.validateName(value),
+              fieldController: lastNameController,
+            ),
+            PhoneField(
+                countryCodes: (country) {
+                  _countryCodes = country.dialCode;
+                  print("Country ${_countryCodes + phoneController.text}");
+                },
+                fieldIcon: const Icon(Icons.phone, size: 18),
+                phoneNumber: phoneController),
+            CustomFormField(
+              fieldIcon: const Icon(Icons.remove_red_eye, size: 18),
+              inputType: TextInputType.number,
+              hintText: 'Pin',
+              maximumLength: 4,
+              validators: (value) => Validators.validatePin(value),
+              fieldController: pinController,
+            ),
+            CustomFormField(
+              maximumLength: 4,
+              inputType: TextInputType.number,
+              fieldIcon: const Icon(Icons.remove_red_eye, size: 18),
+              hintText: 'Confirm Password',
+              validators: (value) => Validators.validatePin(value),
+              fieldController: confirmPin,
+            ),
+            LoginButton(
+              text: 'Register now',
+              validate: () => _formKey.currentState!.validate()
+                  ? Navigator.pushNamed(context, '/otp')
+                  : null,
+              actionField: () {
+                if (_formKey.currentState!.validate()) {
+                  print(
+                      "This is the value ${_countryCodes + pinController.text}");
+                }
+              },
+            ),
+            HyperLinkText(
+              directingText: 'Login instead',
+              actions: () => Navigator.pushReplacementNamed(context, '/login'),
+            ),
+            const CustomDevider(middleText: 'Or sign up with'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircledLogo(
+                    logo: 'assets/images/categories/emailIcon.png',
+                    navigateTo: () =>
+                        Navigator.pushReplacementNamed(context, ("/signup")),
+                  ),
+                  CircledLogo(
+                    navigateTo: () {},
+                    logo: 'assets/images/categories/googleIcon.png',
+                  ),
+                  CircledLogo(
+                    navigateTo: () {},
+                    logo: 'assets/images/categories/appleIcon.png',
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    ), paddingSize: 150,);
+      paddingSize: 150,
+    );
   }
 }
--- lib/screens/auth_screens/widgets/custom_devider.dart
+++ lib/screens/auth_screens/widgets/custom_devider.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 class CustomDevider extends StatelessWidget {
   final String middleText;
-  const CustomDevider({required this.middleText,Key? key}) : super(key: key);
+
+  const CustomDevider({required this.middleText, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-      child: Row(children:  <Widget>[
+      child: Row(children: <Widget>[
         const Expanded(
             child: Divider(
           thickness: 2,
         )),
-        Text(middleText),
+        Padding(
+          padding: const EdgeInsets.only(left: 3, right: 3),
+          child: Text(middleText),
+        ),
         const Expanded(
             child: Divider(
           thickness: 2,
