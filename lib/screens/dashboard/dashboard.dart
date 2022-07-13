aff01e14f66643423831565146044cb009f66766
mucyorene <renemucyomucici@gmail.com>
2022-07-13 18:22:02 +0200

    dashboard refactored and it is now responsive

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:kudibooks_app/models/transition_chart.dart';
 import 'package:kudibooks_app/screens/dashboard/account_transfer.dart';
 import 'package:kudibooks_app/screens/dashboard/all_transaction.dart';
@@ -29,6 +28,54 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
+    List actions = [
+      ActionCard(
+        actionClick: () => Navigator.push(
+            context, CupertinoPageRoute(builder: (ctx) => ProductSale())),
+        cardIcon: const Icon(
+          Icons.shopping_cart_outlined,
+          color: Colors.white,
+        ),
+        cardColor: const Color(0xff157253),
+        title: 'Sell',
+      ),
+      ActionCard(
+        actionClick: () => Navigator.push(
+            context, CupertinoPageRoute(builder: (ctx) => NewInventory())),
+        cardIcon: const Icon(
+          Icons.shopping_cart_outlined,
+          color: Colors.white,
+        ),
+        cardColor: const Color(0xffA70C4A),
+        title: 'New Load',
+      ),
+      ActionCard(
+        actionClick: () => Navigator.push(
+            context, CupertinoPageRoute(builder: (ctx) => NewExpense())),
+        cardIcon: const Icon(
+          Icons.shopping_cart_outlined,
+          color: Colors.white,
+        ),
+        cardColor: const Color(0xffFDAB30),
+        title: 'Expenses',
+      ),
+      ActionCard(
+        actionClick: () => showModalBottomSheet(
+            isDismissible: true,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15),
+                    topLeft: Radius.circular(15))),
+            context: context,
+            builder: (context) => _modalForMore(context)),
+        cardIcon: const Icon(
+          Icons.shopping_cart_outlined,
+          color: Colors.white,
+        ),
+        cardColor: const Color(0xff61B76B),
+        title: 'More',
+      ),
+    ];
     return Scaffold(
       extendBodyBehindAppBar: true,
       drawer: Drawers(dashboardScreen: widget.callBack),
@@ -104,134 +151,20 @@ class _DashboardState extends State<Dashboard> {
                       ),
                     ),
                     Container(
+                      height: 73,
                       margin: const EdgeInsets.only(left: 10, right: 10),
-                      child: Row(
-                        children: [
-                          ActionCard(
-                            cardIcon: const Icon(
-                              Icons.shopping_cart_outlined,
-                              color: Colors.white,
-                            ),
-                            cardColor: const Color(0xff157253),
-                            title: 'Sell',
-                            actionClick: () => Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                    builder: (context) => ProductSale())),
-                          ),
-                          ActionCard(
-                            actionClick: () => Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                    builder: (context) => NewInventory())),
-                            cardIcon: const Icon(
-                              Icons.shopping_cart_outlined,
-                              color: Colors.white,
-                            ),
-                            cardColor: const Color(0xffA70C4A),
-                            title: 'New Load',
-                          ),
-                          ActionCard(
-                            actionClick: () => Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                    builder: (context) => NewExpense())),
-                            cardIcon: const Icon(
-                              Icons.shopping_cart_outlined,
-                              color: Colors.white,
-                            ),
-                            cardColor: const Color(0xffFDAB30),
-                            title: 'Expenses',
-                          ),
-                          ActionCard(
-                            actionClick: () => showModalBottomSheet(
-                                isDismissible: true,
-                                shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(15),
-                                        topLeft: Radius.circular(15))),
-                                context: context,
-                                builder: (context) => SizedBox(
-                                      height: 300,
-                                      child: Column(
-                                        children: [
-                                          ListTile(
-                                            title: const Text(
-                                              "More menus",
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            trailing: IconButton(
-                                                onPressed: () =>
-                                                    Navigator.pop(context),
-                                                icon: const Icon(Icons.close)),
-                                          ),
-                                          ListTile(
-                                            onTap: () => Navigator.push(
-                                                context,
-                                                CupertinoPageRoute(
-                                                    builder: (context) =>
-                                                        InventoryDeduction())),
-                                            leading: const Icon(Icons
-                                                .create_new_folder_outlined),
-                                            title: const Text(
-                                              "Inventory reduction",
-                                            ),
-                                            trailing: const Icon(
-                                              Icons.arrow_forward_ios,
-                                            ),
-                                          ),
-                                          ListTile(
-                                            onTap: () => Navigator.push(
-                                                context,
-                                                CupertinoPageRoute(
-                                                    builder: (context) =>
-                                                        ClientDeposit())),
-                                            leading:
-                                                const Icon(Icons.credit_card),
-                                            title: const Text(
-                                              "Client deposit",
-                                            ),
-                                            trailing: const Icon(
-                                              Icons.arrow_forward_ios,
-                                            ),
-                                          ),
-                                          const ListTile(
-                                            leading: Icon(Icons.credit_card),
-                                            title: Text(
-                                              "Vendor deposit",
-                                            ),
-                                            trailing: Icon(
-                                              Icons.arrow_forward_ios,
-                                            ),
-                                          ),
-                                          ListTile(
-                                            onTap: () => Navigator.push(
-                                                context,
-                                                CupertinoPageRoute(
-                                                    builder: (context) =>
-                                                        AccountTransfer())),
-                                            leading: const Icon(Icons
-                                                .create_new_folder_outlined),
-                                            title: const Text(
-                                              "Account transfer",
-                                            ),
-                                            trailing: const Icon(
-                                              Icons.arrow_forward_ios,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    )),
-                            cardIcon: const Icon(
-                              Icons.shopping_cart_outlined,
-                              color: Colors.white,
-                            ),
-                            cardColor: const Color(0xff61B76B),
-                            title: 'More',
-                          ),
-                        ],
+                      child: LimitedBox(
+                        maxHeight: 100,
+                        maxWidth: MediaQuery.of(context).size.width,
+                        child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, inx) {
+                              return actions[inx];
+                            },
+                            separatorBuilder: (_, ins) => const SizedBox(
+                                  width: 3,
+                                ),
+                            itemCount: actions.length),
                       ),
                     ),
                     Container(
@@ -270,46 +203,45 @@ class _DashboardState extends State<Dashboard> {
                       child: ListView.separated(
                           physics: const NeverScrollableScrollPhysics(),
                           itemBuilder: (context, index) => ListTile(
-                            subtitle: const Text("20 April 2022"),
-                            trailing: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment:
-                                  CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "\$25.99",
+                                subtitle: const Text("20 April 2022"),
+                                trailing: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "\$25.99",
+                                    ),
+                                    Text(
+                                      index % 2 == 0
+                                          ? "Approved"
+                                          : "In process",
+                                      style: TextStyle(
+                                        color: index % 2 == 0
+                                            ? Colors.green
+                                            : Colors.amber,
+                                      ),
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  index % 2 == 0
-                                      ? "Approved"
-                                      : "In process",
+                                title: const Text(
+                                  "Transaction name",
                                   style: TextStyle(
-                                    color: index % 2 == 0
-                                        ? Colors.green
-                                        : Colors.amber,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14),
+                                ),
+                                leading: CircleAvatar(
+                                  backgroundColor: const Color(0xffC4C4C4),
+                                  child: Text(
+                                    "${++index}",
+                                    style: TextStyle(
+                                        color: index % 2 == 0
+                                            ? Colors.amber
+                                            : Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
                                   ),
-                                )
-                              ],
-                            ),
-                            title: const Text(
-                              "Transaction name",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14),
-                            ),
-                            leading: CircleAvatar(
-                              backgroundColor: const Color(0xffC4C4C4),
-                              child: Text(
-                                "${++index}",
-                                style: TextStyle(
-                                    color: index % 2 == 0
-                                        ? Colors.amber
-                                        : Colors.green,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
+                                ),
                               ),
-                            ),
-                          ),
                           separatorBuilder: (_, idx) => const SizedBox(
                                 height: 5,
                               ),
@@ -318,6 +250,75 @@ class _DashboardState extends State<Dashboard> {
                   ],
                 ),
               ))
+        ],
+      ),
+    );
+  }
+
+  _modalForMore(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: Column(
+        children: [
+          ListTile(
+            title: const Text(
+              "More menus",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(Icons.close)),
+          ),
+          ListTile(
+            onTap: () => Navigator.push(
+                context,
+                CupertinoPageRoute(
+                    builder: (context) => InventoryDeduction())),
+            leading: const Icon(Icons.create_new_folder_outlined),
+            title: const Text(
+              "Inventory reduction",
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ),
+          ListTile(
+            onTap: () => Navigator.push(
+                context,
+                CupertinoPageRoute(
+                    builder: (context) => ClientDeposit())),
+            leading: const Icon(Icons.credit_card),
+            title: const Text(
+              "Client deposit",
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ),
+          const ListTile(
+            leading: Icon(Icons.credit_card),
+            title: Text(
+              "Vendor deposit",
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ),
+          ListTile(
+            onTap: () => Navigator.push(
+                context,
+                CupertinoPageRoute(
+                    builder: (context) => AccountTransfer())),
+            leading: const Icon(Icons.create_new_folder_outlined),
+            title: const Text(
+              "Account transfer",
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ),
         ],
       ),
     );
