8c9ca95ae75cff252602e2eca9d951ec80bbd05b
Elvin-HIRWA <uyisaristide@gmail.com>
2022-07-22 15:02:23 +0200

    all that i have before presentation

--- lib/screens/dashboard/classes/pie_chart_data.dart
+++ lib/screens/dashboard/classes/pie_chart_data.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 
 class PieData {
   static List<Data> data = [
-    Data(month: '20%', color: Colors.orange, percent: 40),
-    Data(month: '35%', color: Colors.teal, percent: 30),
-    Data(month: '25%', color: Colors.yellow, percent: 20),
+    Data(month: '40%', color: Colors.orange, percent: 40),
+    Data(month: '30%', color: Colors.teal, percent: 30),
+    Data(month: '20%', color: Colors.yellow, percent: 20),
     Data(month: '10%', color: Colors.red, percent: 10),
     // Data(month: 'aprl', color: Colors.green, percent: 10),
   ];
@@ -16,4 +16,5 @@ class Data {
   final Color color;
   final double percent;
 
-  Data({required this.month, required this.color, required this.percent});}
+  Data({required this.month, required this.color, required this.percent});
+}
